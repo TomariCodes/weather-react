01e2d43e088d68db2e8c8c3054b7be5f7b525150
Tomari <tomari7878@gmail.com>
2026-07-20 16:29:39 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,31 +3,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            className="footer_link"
-            href="https://tomari-southern.netlify.app/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tomari Southern
-          </a>{" "}
-          and is open-sourced on{" "}
-          <a
-            className="footer_link"
-            href="https://github.com/TomariCodes/weather-react"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+          <button className="btn btn-primary">Hello</button>
+        </header>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              className="footer_link"
+              href="https://tomari-southern.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tomari Southern
+            </a>{" "}
+            and is open-sourced on{" "}
+            <a
+              className="footer_link"
+              href="https://github.com/TomariCodes/weather-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
